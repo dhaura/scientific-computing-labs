992d62a48e02922bb17c0492d35677db5738f8ae
dhaura <dhaurapathirana@gmail.com>
2023-04-02 23:51:43 +0530

    update linux shell script assignment 1

--- assignment-1/script-linux.sh
+++ assignment-1/script-linux.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # Define an array of arguments
-args=(10 100 500 1000 5000 10000)
+args=(10 100 500 1000)
 
 # Loop through the arguments and run the program with each one
 for arg in "${args[@]}"
 do
+    echo "FLOAT n = $arg"
+    MEM=`free | awk 'NR==2{print $7}'`
+    echo "Available main memory: $MEM bytes"
     ./matMulFloat "$arg" "20" >> results/float/results-float-n-"$arg".txt
+    
+    echo "INT n = $arg"
+    MEM=`free | awk 'NR==2{print $7}'`
+    echo "Available main memory: $MEM bytes"
     ./matMulInt "$arg" "20" >> results/int/results-int-n-"$arg".txt
 done
